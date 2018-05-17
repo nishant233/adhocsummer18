c3876c3ac62385790a5bd34e0e839397991f6e4a
nishant233 <nishantk093@gmail.com>
2018-05-17 18:02:40 +0530

    sever

--- rec.py
+++ rec.py
@@ -1,6 +1,6 @@
 #! usr/bin/env python2	
 import socket
-myip="192.168.10.36"
+myip="127.0.0.1"
 myport=9999
 
 s=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -9,4 +9,4 @@ while 4:
 	data=s.recvfrom(1000)
 	print data[0]
 	r=raw_input('type to reply ')
-	s.sendo(rply,data[1])
+	s.sendto(rply,data[1])
