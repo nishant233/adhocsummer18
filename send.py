4d050c3a0a83526a6e7370deb0003702ebf5a5d3
nishant233 <nishantk093@gmail.com>
2018-05-18 17:14:08 +0530

    updated one

--- send.py
+++ send.py
@@ -2,5 +2,8 @@
 
 import socket
 s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-msg=raw_input("Enter your message :")
-s.sendto(msg,("127.0.0.1",9999))
+while 6:
+	msg=raw_input("Enter your message :")
+	s.sendto(msg,("127.0.0.1",9999))
+	data=s.recvfrom(1000)
+	print data[0]
